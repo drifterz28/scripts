8471ca7f12dd5782c83d0fb7f8f862d56254c648
Chris Whitney <cwhitney@kollective.com>
2019-12-02 21:51:50 -0800

    Update image zoom

--- zoom/index.js
+++ zoom/index.js
@@ -1,43 +1,138 @@
-const images = document.querySelectorAll('.images img');
+const images = document.querySelectorAll('.pirobox_gall');
 const zoom = document.querySelector('.image-zoom');
-const lightBox = document.querySelector('.light-box');
 const lightBoxShade = document.querySelector('.light-box-shade');
+const lightBox = document.querySelector('.light-box');
+const extraImageNav = document.querySelectorAll('.extra-images-nav');
+const extraImages = document.querySelector('.ex-images');
+const lightBoxExtraImages = document.querySelector('.light-box-ex-image');
+const lightBoxNav = document.querySelectorAll('.images-nav');
 
-const getPos = (e) => {
+const imageCount = images.length;
+let imageIndex;
+let extraImageSlidePos = 0;
+const exImageWidth = (imageCount -1) * 110;
+const steps = Math.ceil(exImageWidth / 200);
+
+const getPos = function(e) {
   const rect = e.target.getBoundingClientRect();
   const x = (e.clientX - rect.left) / rect.width * 100;
   const y = (e.clientY - rect.top) / rect.height * 100;
-  zoom.style.backgroundPosition = `${x}% ${y}%`;
+  zoom.style.backgroundPosition = x + '% ' + y + '%';
 }
 
-const addImage = (e) => {
+const addImage = function(e) {
   const target = e.target;
   removeImage();
-  const image = target.getAttribute('src');
-  zoom.style.backgroundImage = `url(${image})`;
+  const image = target.getAttribute('href');
+  zoom.style.display = 'block';
+  zoom.style.backgroundImage = 'url(' + image +')';
 }
 
-const removeImage = () => {
+const removeImage = function() {
   zoom.style.backgroundImage = 'none';
+  zoom.style.display = 'none';
 }
 
-const showImage = (e) => {
+const showImageEvent = function(e) {
   e.preventDefault();
   const target = e.target;
-  const image = target.getAttribute('src');
-  lightBox.style.backgroundImage = `url(${image})`;
+  showImage(target);
+}
+
+const changeSelected = function() {
+  const selected = document.querySelector('.selected');
+  if(selected) {
+    selected.classList.remove('selected');
+  }
+  document.querySelector('.light-box-images[index=\'' + imageIndex + '\']').classList.add('selected');
+}
+
+const showImage = function(elm) {
+  const imageElm = elm.getAttribute('href') ? elm : elm.parentNode;
+  imageIndex = imageElm.getAttribute('index');
+  changeSelected();
+  lightBox.querySelector('img').src = imageElm.getAttribute('href');
   lightBox.classList.add('show');
 }
 
-const closeLightBox = () => {
-  lightBox.innerHTML = '';
+const closeLightBox = function() {
   lightBox.classList.remove('show');
 }
 
-lightBox.addEventListener('click', closeLightBox);
-[...images].forEach((image) => {
-  image.addEventListener('mouseenter', addImage);
-  image.addEventListener('mouseleave', removeImage);
-  image.addEventListener('click', showImage);
-  image.addEventListener('mousemove', getPos);
-});
+const slideExtraImages = function(e) {
+  const target = e.target;
+  const isBack = target.classList.contains('back');
+  const disable = document.querySelector('.disable');
+  if(disable) {
+    disable.classList.remove('disable');
+  }
+
+  if(isBack && extraImageSlidePos > 0) {
+    extraImageSlidePos = extraImageSlidePos - 220;
+  } else if(!isBack) {
+    extraImageSlidePos = extraImageSlidePos + 220;
+  }
+  if(extraImageSlidePos <= 0) {
+    extraImageSlidePos = 0;
+    extraImageNav[0].classList.add('disable');
+  }
+  if(extraImageSlidePos > (exImageWidth - 240)) {
+    extraImageSlidePos = exImageWidth - 240;
+    extraImageNav[1].classList.add('disable');
+  }
+  extraImages.style.transform = 'translateX(-' + extraImageSlidePos + 'px)';
+}
+
+const lightBoxImages = function(e) {
+  e.preventDefault();
+  showImage(e.target);
+}
+
+const changeLightBoxImage = function(e) {
+  const isBack = e.target.classList.contains('back');
+  if(isBack) {
+    imageIndex--;
+  } else {
+    imageIndex++;
+  }
+  if(imageIndex < 0) {
+    imageIndex = imageCount - 1;
+  }
+  if(imageIndex >= imageCount) {
+    imageIndex = 0;
+  }
+  const nextImage = document.querySelector('.light-box-images[index=\'' + imageIndex + '\']')
+  showImage(nextImage);
+}
+
+if(imageCount) {
+  document.querySelector('.light-box-close').addEventListener('click', closeLightBox);
+  lightBoxShade.addEventListener('click', closeLightBox);
+
+  for(let i = 0; i < imageCount; i++) {
+    images[i].setAttribute('index', i);
+    images[i].addEventListener('mouseenter', addImage);
+    images[i].addEventListener('mouseleave', removeImage);
+    images[i].addEventListener('click', showImageEvent);
+    images[i].addEventListener('mousemove', getPos);
+    const clone = images[i].cloneNode(true);
+    clone.classList.remove('pirobox_gall');
+    clone.classList.add('light-box-images');
+    lightBoxExtraImages.appendChild(clone);
+    clone.addEventListener('click', lightBoxImages);
+  }
+
+  if(imageCount > 1) {
+    lightBoxNav[0].addEventListener('click', changeLightBoxImage);
+    lightBoxNav[1].addEventListener('click', changeLightBoxImage);
+  } else {
+    lightBoxNav[0].classList.add('hide-nav');
+    lightBoxNav[1].classList.add('hide-nav');
+    lightBoxExtraImages.classList.add('hide-nav');
+  }
+
+  if(imageCount > 5) {
+    extraImageNav[0].addEventListener('click', slideExtraImages);
+    extraImageNav[1].addEventListener('click', slideExtraImages);
+  }
+}
